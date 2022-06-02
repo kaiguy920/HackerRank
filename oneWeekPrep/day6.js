50f956fb4ee0b6f8ce75a1053880882fa7e3367d
kaiguy920 <smithkai920@gmail.com>
2022-06-02 13:15:37 -0400

    solves grade round problem

--- oneWeekPrep/day6.js
+++ oneWeekPrep/day6.js
@@ -4,23 +4,52 @@
 // given an array of grades return the rounded grade given these rules:
 // if grade minus rounded up to next 5 integer is less than 3, round to next 5 integer
 // score less than 40 is failig, do not round 
+// this solves rounding to nearest 5 integer
 function gradingStudents(grades) {
     // Write your code here
     const roundedGrades = []
     for (let i = 0; i < grades.length; i++) {
+        let near = Math.round(grades[i] / 5) * 5
         if (grades[i] <= 40) {
             roundedGrades.push(grades[i])
         } else {
-            if (Math.round((grades[i] / 5) * 5) - grades[i] > 3) {
-                roundedGrades.push(Math.round((grades[i] / 5) * 5))
+            console.log("near", near)
+            console.log(Math.abs(near - grades[i]));
+            let distance = Math.abs(near - grades[i])
+            if (distance < 3) {
+                roundedGrades.push(near)
             } else {
                 roundedGrades.push(grades[i])
             }
         }
     }
     console.group(roundedGrades)
-    // function roundNearest5(num) {
-    //     return Math.round(num / 5) * 5;
-    //   }
+
 }
-gradingStudents([29, 84, 57])
+gradingStudents([73, 67, 38, 33])
+// should return [75, 67, 40, 33]
+
+// to round grade to next higher # increment of 5
+
+function gradingStudentss(grades) {
+    // Write your code here
+    const roundedGrades = []
+    for (let i = 0; i < grades.length; i++) {
+        let near = Math.ceil(grades[i] / 5) * 5
+        if (grades[i] <= 40) {
+            roundedGrades.push(grades[i])
+        } else {
+            console.log("near", near)
+            console.log(Math.abs(near - grades[i]));
+            let distance = near - grades[i]
+            if (distance < 3) {
+                roundedGrades.push(near)
+            } else {
+                roundedGrades.push(grades[i])
+            }
+        }
+    }
+    console.group("2", roundedGrades)
+
+}
+gradingStudentss([73, 67, 38, 33])
