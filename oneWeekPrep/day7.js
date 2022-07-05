3282e8ebf5ce22748ffb010107e986b0ce5c94b7
kaiguy920 <smithkai920@gmail.com>
2022-07-05 18:45:33 -0400

    solves counter game function

--- oneWeekPrep/day7.js
+++ oneWeekPrep/day7.js
@@ -9,8 +9,8 @@
 const pageCount = (num, p) => {
     const frontTurn = Math.round((p - 1) / 2)
     const backTurn = Math.round(((num - 1) - p) / 2)
-    console.log("back", backTurn);
-    console.log("front", frontTurn);
+    // console.log("back", backTurn);
+    // console.log("front", frontTurn);
     if (frontTurn <= backTurn) {
         return frontTurn
     } else {
@@ -19,3 +19,36 @@ const pageCount = (num, p) => {
 
 }
 pageCount(15, 5)
+
+// ===========================================================
+//                      NUMBER 2
+// ===========================================================
+// Louise and Richard have developed a numbers game. They pick a number and check to see if it is a power of 2. If it is, they divide it by 2. If not, they reduce it by the next lower number which is a power of 2. Whoever reduces the number to 1 wins the game. Louise always starts.
+// ex: n = 132
+// Louise goes first; 132 is not a power of 2; the next lower power of 2 is 128, so she subtracts that from 132 & passes 4 to Richard. 4 is a power of 2, so Richard divides it by 2 & passes 2 to Louise. 2 is a power, so Louise divides by 2 & reaches 1, winning the game
+function counterGame(n) {
+    // Write your code here
+    if (n == 1) return 'Richard';
+    if (n == 2) return 'Louise';
+    let count = 0;
+    let move = 0;
+    while (n > 1) {
+        let power = parseInt(Math.log2(n));
+        let max = 2 ** power;
+        if (n === max) {
+            n = parseInt(n / 2);
+        } else {
+            n = n - max;
+        }
+        move++;
+    }
+    if (move % 2 === 0) {
+        console.log("Richard")
+        // return 'Richard';
+    } else {
+        console.log("Louise")
+        // return 'Louise';
+    }
+
+}
+counterGame(6)
