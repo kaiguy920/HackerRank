1dc02a6a214c07da508f86660e764881c9226ee4
kaiguy920 <smithkai920@gmail.com>
2022-08-04 15:04:59 -0400

    solves highest & lowest function

---
+++ CodeWars/highLow.js
@@ -0,0 +1,22 @@
+// given a sting of numbers, return the highest & lowest #
+
+// function highAndLow(numbers) {
+//     const ascendNums = numbers.sort(function (a, b) { return a - b })
+//     let len = numbers.length - 1
+//     console.log(len)
+//     const highLow = [ascendNums[0], ascendNums[len]]
+//     console.log(highLow)
+// }
+
+// console.log(highAndLow([1, 2, -3, 4, 5]))
+
+
+function highAndLow(numbers) {
+    const num = numbers.split(" ")
+    let sorted = num.sort(function (a, b) { return a - b })
+    console.log(sorted)
+    let last = sorted.length - 1
+    return sorted[last] + " " + sorted[0]
+}
+
+console.log(highAndLow("1 2 3 4 5"))
