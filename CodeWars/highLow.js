c470b4de963284be87d64b47cd94411fce0f813f
kaiguy920 <smithkai920@gmail.com>
2022-08-04 15:58:35 -0400

    solves descending order function

--- CodeWars/highLow.js
+++ CodeWars/highLow.js
@@ -14,9 +14,22 @@
 function highAndLow(numbers) {
     const num = numbers.split(" ")
     let sorted = num.sort(function (a, b) { return a - b })
-    console.log(sorted)
+    // console.log(sorted)
     let last = sorted.length - 1
     return sorted[last] + " " + sorted[0]
 }
 
 console.log(highAndLow("1 2 3 4 5"))
+
+// =======================================================================
+//                          DESCENDING ORDER
+// =======================================================================
+function descendingOrder(n) {
+    const split = n.toString().split("")
+    // console.log(split);
+    let sorted = split.sort(function (a, b) { return b - a }).join("")
+    let num = parseInt(sorted)
+    // console.log("num", typeof num);
+    return num
+}
+console.log(descendingOrder(1021));
