4815128ddd6ff4e76e89f56dfd42a260843ea557
kaiguy920 <smithkai920@gmail.com>
2022-08-05 17:44:58 -0400

    adds better solution to accum

--- CodeWars/accum.js
+++ CodeWars/accum.js
@@ -2,17 +2,29 @@
 // accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 
 function accum(s) {
-    let newString = []
-    for (let i = 0; i < s.length; i++) {
-        let repeat = s.indexOf(s[i])
-        newString.push(s[i].toLowerCase().repeat(repeat + 1))
+    // let newString = []
+    // for (let i = 0; i < s.length; i++) {
+    //     let repeat = s.indexOf(s[i])
+    //     newString.push(s[i].toLowerCase().repeat(repeat + 1))
+    // }
+    // let words = newString.toString().split(",")
+    // for (let i = 0; i < words.length; i++) {
+    //     words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+    // }
+    // // console.log(words.toString().replace(/,/g, '-'));
+    // return words.toString().replace(/,/g, '-')
+    const result = []
+    const lowerStr = s.toLowerCase()
+
+    for (let i = 0; i < lowerStr.length; i++) {
+        let str = lowerStr[i].toUpperCase()
+        for (let j = 0; j < i; j++) {
+            str += lowerStr[i]
+        }
+        result.push(str)
     }
-    let words = newString.toString().split(",")
-    for (let i = 0; i < words.length; i++) {
-        words[i] = words[i][0].toUpperCase() + words[i].substring(1);
-    }
-    // console.log(words.toString().replace(/,/g, '-'));
-    return words.toString().replace(/,/g, '-')
+
+    return result.join("-")
 
 }
 console.log(accum("ZpglnRxqenU"));
