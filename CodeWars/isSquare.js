219c4d1cd5d21aa4d43ce7044822ed0935436a6f
kaiguy920 <smithkai920@gmail.com>
2022-08-19 17:47:00 -0400

    solves x & o problem

--- CodeWars/isSquare.js
+++ CodeWars/isSquare.js
@@ -6,4 +6,27 @@ const isSquare = (n) => {
         return false
     }
 }
-console.log(isSquare('4'));
+// console.log(isSquare('4'));
+
+// =======================================================================
+//                          X & O
+// =======================================================================
+// Check to see if a string has the same amount of 'x's and 'o's. 
+function XO(str) {
+    let strLower = str.toLowerCase()
+    let x = 0
+    let o = 0
+    for (let i = 0; i < strLower.length; i++) {
+        if (!strLower[i] == "x" || !strLower[i] == "o") {
+            return false
+        } else if (strLower[i] === "x") {
+            x++
+        } else if (strLower[i] === "o") {
+            o++
+        }
+    }
+
+    return x == o
+
+}
+console.log(XO("oxXXOo"));
