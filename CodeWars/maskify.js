c888811e864bed9b361efae47d082da6d46d7090
kaiguy920 <smithkai920@gmail.com>
2022-08-24 16:37:45 -0400

    adds multiples of 3 & 5 solution

--- CodeWars/maskify.js
+++ CodeWars/maskify.js
@@ -11,3 +11,31 @@ function maskify(cc) {
 }
 
 console.log(maskify("4556364607935616"))
+
+// ===============================================================
+//                      MULTIPLES OF 3 OR 5
+// ===============================================================
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+function solution(number) {
+    let num = number - 1
+    let sum = 0
+    let numCount = []
+
+    for (let i = 1; i <= num; i++) {
+        numCount.push(i)
+    }
+    console.log(numCount);
+    if (number <= 0) {
+        return 0
+    } else {
+        for (let i = 0; i < numCount.length; i++) {
+            if (numCount[i] % 3 === 0 || numCount[i] % 5 === 0) {
+                sum += numCount[i]
+            }
+        }
+        return sum
+    }
+}
+console.log(solution(10))
