a6a1af9b180fa43223de61807b61fdb057c18c20
kaiguy920 <smithkai920@gmail.com>
2022-08-31 16:36:10 -0400

    adds another solution to consec strings function

--- CodeWars/consecStrings.js
+++ CodeWars/consecStrings.js
@@ -1,22 +1,30 @@
 // You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array
 function longestConsec(strarr, k) {
-    if (k <= 0) {
+    if (k <= 0 || strarr.length < k || strarr.length == 0)
         return ""
-    } else if (strarr.length < k) {
-        return ""
-    } else {
 
-        let ascendLen = strarr.sort((a, b) => a.length - b.length).reverse()
-        console.log("Ascend", ascendLen);
-        let concat = []
-        for (let i = 0; i < ascendLen.length; i++) {
-            concat.push(ascendLen[i])
-            if (i === k - 1) { break }
+    // ========================SOLUTION 1===================================
+    // let ascendLen = strarr.sort((a, b) => a.length - b.length).reverse()
+    // console.log("Ascend", ascendLen);
+    // let concat = []
+    // for (let i = 0; i < ascendLen.length; i++) {
+    //     concat.push(ascendLen[i])
+    //     if (i === k - 1) { break }
+    // }
+    // return concat.join("")
+    // ========================SOLUTION 2===================================
+    let longStr = '';
+    let newStr = '';
+    for (let i = 0; i < strarr.length; i++) {
+        newStr = strarr.slice(i, i + k);
+        if (newStr.join('').length > longStr.length) {
+            longStr = newStr.join('');
         }
-        return concat.join("")
     }
-
+    return longStr
 }
+
+
 // console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2)); // ==> "abigailtheta"
 
 console.log(longestConsec(["itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2));
