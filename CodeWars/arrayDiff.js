6ccc53aaf2afba796b12d37ff793bd984541563c
kaiguy920 <smithkai920@gmail.com>
2022-09-22 15:42:11 -0400

    adds random generator function

--- CodeWars/arrayDiff.js
+++ CodeWars/arrayDiff.js
@@ -5,3 +5,22 @@ function arrayDiff(a, b) {
     return a
 }
 console.log(arrayDiff([1, 2, 2, 2, 3], [2])) // ==>[1, 3]
+
+// =========================================================
+//                          RANDOM
+// =========================================================
+// program to generate random strings
+// declare all characters
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
+
+console.log(generateString(5));
